5551cefa132a1063e68395935a419900cb897c75
hwangwoojin <hwangwoojin@hwangwoojins-MacBook-Pro.local>
2020-09-08 16:37:08 +0900

    [숫자카드게임] min/max 함수를 사용할수도 있다

--- CHAPTER 03 그리디/숫자카드게임.py
+++ CHAPTER 03 그리디/숫자카드게임.py
@@ -13,11 +13,9 @@ for i in range(N):
 # 각 행에서 가장 작은 숫자를 뽑는다.
 num_list = []
 for i in range(N):
-    matrix[i].sort()
-    num_list.append(matrix[i][0])
+    num_list.append(min(matrix[i]))
 # 그 중에서 가장 큰 숫자를 선택한다.
-num_list.sort()
-result = num_list[N-1]
+result = max(num_list)
 
 # 답을 출력한다.
 print(result)
