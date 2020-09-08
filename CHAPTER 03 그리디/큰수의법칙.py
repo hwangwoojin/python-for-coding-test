5ae168486a4ad6d2ebd037912b0778dc1896c5fd
hwangwoojin <hwangwoojin@hwangwoojins-MacBook-Pro.local>
2020-09-08 16:03:39 +0900

    [큰수의법칙] 정렬을 사용하면 가장 큰 수와 두번째 큰 수를 쉽게 찾을 수 있다.

--- CHAPTER 03 그리디/큰수의법칙.py
+++ CHAPTER 03 그리디/큰수의법칙.py
@@ -9,15 +9,10 @@ N, M, K = map(int, input().split())
 num_list = list(map(int, input().split()))
 
 # 가장 큰 수를 찾는다.
-max1, max1_index = 0, 0
-for i in range(len(num_list)):
-    if max1 < num_list[i]:
-        max1, max1_index = num_list[i], i
 # 그 다음 큰 수를 찾는다.
-max2, max2_index = 0, 0
-for i in range(len(num_list)):
-    if max2 < num_list[i] and i != max1_index:
-        max2, max2_index = num_list[i], i
+num_list.sort()
+max1 = num_list[N - 1]
+max2 = num_list[N - 2]
 
 # 가장 큰 수를 K 번 더한다.
 # 그 다음 큰 수를 1 번 더한다.
