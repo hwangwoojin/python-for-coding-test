55a28a7e235e30a56f915d066557e91ab2c4455c
hwangwoojin <hwangwoojin@hwangwoojins-MacBook-Pro.local>
2020-09-08 16:13:49 +0900

    [큰수의법칙] 수열을 파악하면 반복문을 없앨수도 있다.

--- CHAPTER 03 그리디/큰수의법칙.py
+++ CHAPTER 03 그리디/큰수의법칙.py
@@ -14,18 +14,13 @@ num_list.sort()
 max1 = num_list[N - 1]
 max2 = num_list[N - 2]
 
-# 가장 큰 수를 K 번 더한다.
-# 그 다음 큰 수를 1 번 더한다.
-# 이를 M 번 될때까지 반복한다.
-result, count_k = 0, 0
-for i in range(M):
-    if count_k == K:
-        result += max2
-        count_k = 0
-        continue
-    else:
-        result += max1
-        count_k += 1
+# 가장 큰 수가 더해지는 횟수를 구한다.
+# 그 다음 큰 수가 더해지는 횟수를 구한다.
+count_max2 = M // (K + 1)
+count_max1 = M - count_max2
+
+# 결과를 구한다.
+result = max1 * count_max1 + max2 * count_max2
 
 # 첫째 줄에 답을 출력한다.
 print(result)
