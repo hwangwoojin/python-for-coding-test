8baa6e679102a597a943f644d62309703f7a549d
hwangwoojin <hwangwoojin@hwangwoojins-MacBook-Pro.local>
2020-09-09 21:53:19 +0900

    [왕실의나이트] 만약 체스판 좌표를 숫자로만 구할 수 있다면 반복문을 줄일 수 있다.

--- CHAPTER 04 구현/왕실의나이트.py
+++ CHAPTER 04 구현/왕실의나이트.py
@@ -4,27 +4,18 @@
 
 # 현재 나이트의 좌표가 입력된다.
 location = input()
-x = location[0]
-y = location[1]
-
-# 체스판
-nx = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-ny = ['1', '2', '3', '4', '5', '6', '7', '8']
+x = int(ord(location[0]) - int(ord('a'))) + 1
+y = int(location[1])
 
 # 나이트가 움직이는 거리
-dx = [2, 2, 1, -1, -2, -2, -1, 1]
-dy = [-1, 1, 2, 2, 1, -1, -2, -2]
+steps = [(2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2)]
 
 result = 0
-# 나이트 좌표를 구한다.
-for i in range(len(nx)):
-    for j in range(len(ny)):
-        if nx[i] == x and ny[j] == y:
-            # 나이트가 이동할 수 있는 좌표를 구한다.
-            for k in range(len(dx)):
-                loc_x, loc_y = i + dx[k], j + dy[k]
-                # 그 좌표가 여전히 체스판 안이라면, 결과에 1을 더한다.
-                if 0 <= loc_x < 8 and 0 <= loc_y < 8:
-                    result += 1
+# 나이트가 이동할 수 있는 좌표를 구한다.
+for step in steps:
+    loc_x, loc_y = x + step[0], y + step[1]
+    # 그 좌표가 여전히 체스판 안이라면, 결과에 1을 더한다.
+    if 1 <= loc_x <= 8 and 1 <= loc_y <= 8:
+        result += 1
 
 print(result)
