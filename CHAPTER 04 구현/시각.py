0f9956df5b30b7e80044c75d79585fa767111c28
hwangwoojin <hwangwoojin@hwangwoojins-MacBook-Pro.local>
2020-09-09 21:14:33 +0900

    [시각] 반복문을 사용하면 좀 더 직관적인 풀이가 가능하다.

--- CHAPTER 04 구현/시각.py
+++ CHAPTER 04 구현/시각.py
@@ -5,15 +5,15 @@
 # N 이 입력된다.
 N = int(input())
 
-# 00시 00분 00초애서 0시 59분 59초 까지 시각 중
-# 3이 하나라도 포함되는 경우의 수를 구한다.
-count_3 = 6 * 10 * 6 * 10
-count_not_3 = count_3 - 5 * 9 * 5 * 9
-
-result = (N + 1) * count_not_3
-# 3시가 포함되는 경우
-if N >= 3:
-    result += count_3 - count_not_3
+# 최대 경우가 86400 밖에 되지 않으므로
+# 반복문을 사용하는 것이 더 직관적이다.
+result = 0
+for i in range(N + 1):
+    for j in range(60):
+        for k in range(60):
+            # 3이 포함되면 카운트를 늘린다.
+            if '3' in str(i) + str(j) + str(k):
+                result += 1
 
 # 답을 출력한다.
 print(result)
