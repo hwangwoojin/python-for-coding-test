9beafe8f284c7df0e5df8cdb026396795135f94e
hwangwoojin <hwangwoojin.dev@gmail.com>
2020-09-11 12:54:31 +0900

    [위에서아래로] 첫 풀이

---
+++  CHAPTER 06 정렬/위에서아래로.py
@@ -0,0 +1,18 @@
+# 178p
+# 실전문제
+# 위에서 아래로
+
+# N 이 주어진다.
+N = int(input())
+
+# N 개의 수가 입력된다.
+numbers = []
+for i in range(N):
+    numbers.append(int(input()))
+
+# 내립차순으로 정렬한다.
+numbers.sort()
+result = numbers[::-1]
+
+# 답을 출력한다.
+print(result)
