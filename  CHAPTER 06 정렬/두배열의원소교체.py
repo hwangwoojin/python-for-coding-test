6ea2ae656a779faf0a5bcb97b241e6a728bc1a18
hwangwoojin <hwangwoojin.dev@gmail.com>
2020-09-11 13:18:26 +0900

    [두배열의원소교체] A 의 원소가 B 의 원소보다 커지는 경우 고려해야 한다.

---  CHAPTER 06 정렬/두배열의원소교체.py
+++  CHAPTER 06 정렬/두배열의원소교체.py
@@ -16,6 +16,9 @@ B = list(map(int, input().split()))
 # A 를 오름차순으로 정렬하고 B 를 내림차순으로 정렬했을 때 앞에서 K 개의 원소들이다.
 A, B = sorted(A), sorted(B, reverse=True)
 for i in range(K):
+    # A 의 원소가 B 의 원소보다 크게 되면 종료한다.
+    if A[i] >= B[i]:
+        break
     A[i], B[i] = B[i], A[i]
 
 # 답을 출력한다.
