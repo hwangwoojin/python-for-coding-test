f42d64960f980e6da18e099511fc0edcae72d7db
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-01-19 15:20:09 +0900

    [부품찾기] 집합을 이용한 간단한 풀이

--- CHAPTER 07 이진 탐색/부품찾기.py
+++ CHAPTER 07 이진 탐색/부품찾기.py
@@ -6,20 +6,13 @@ import sys
 
 # N, N 개의 정수, M, M 개의 정수 입력
 N = int(input())
-list_N = list(map(int, sys.stdin.readline().rstrip().split()))
+list_N = set(map(int, sys.stdin.readline().rstrip().split()))
 M = int(input())
 list_M = list(map(int, sys.stdin.readline().rstrip().split()))
 
-# 계수 정렬을 위한 리스트 (1 <= M <= 100000)
-array = [0] * 100001
-
-# list_N 을 계수 정렬을 위한 리스트에 기록한다.
-for i in list_N:
-    array[i] = 1
-
-# 계수 정렬 리스트에서 찾는다.
+# 집합에서 찾는다.
 for i in list_M:
-    if array[i] == 0:
-        print('no')
-    else:
+    if i in list_N:
         print('yes')
+    else:
+        print('no')
