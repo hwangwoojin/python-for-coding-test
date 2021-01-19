ac398bf44c8f40e7743d38a5b835ade785f6e964
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-01-19 15:18:40 +0900

    [부품찾기] 계수 정렬 풀이

--- CHAPTER 07 이진 탐색/부품찾기.py
+++ CHAPTER 07 이진 탐색/부품찾기.py
@@ -10,27 +10,16 @@ list_N = list(map(int, sys.stdin.readline().rstrip().split()))
 M = int(input())
 list_M = list(map(int, sys.stdin.readline().rstrip().split()))
 
-# list_N 을 정렬
-list_N.sort()
+# 계수 정렬을 위한 리스트 (1 <= M <= 100000)
+array = [0] * 100001
 
+# list_N 을 계수 정렬을 위한 리스트에 기록한다.
+for i in list_N:
+    array[i] = 1
 
-# 이진 탐색 재귀 함수
-def bin_search(array, target, start, end):
-    if start > end:
-        return None
-    mid = (start + end) // 2
-    if array[mid] == target:
-        return mid
-    elif array[mid] > target:
-        return bin_search(array, target, start, mid - 1)
-    else:
-        return bin_search(array, target, mid + 1, end)
-
-
-# list_N 에서 list_M 의 각 원소가 존재하는지를 이진 탐색으로 찾는다.
-for i in range(M):
-    target_index = bin_search(list_N, list_M[i], 0, N - 1)
-    if target_index is None:
+# 계수 정렬 리스트에서 찾는다.
+for i in list_M:
+    if array[i] == 0:
         print('no')
     else:
         print('yes')
