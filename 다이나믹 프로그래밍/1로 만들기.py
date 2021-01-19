b93b05e6b32690694fa059a0159af3ff5c24c689
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-01-19 19:38:51 +0900

    [1로 만들기] bottup-up DP 로 해결

--- 다이나믹 프로그래밍/1로 만들기.py
+++ 다이나믹 프로그래밍/1로 만들기.py
@@ -5,24 +5,20 @@
 # 입력
 X = int(input())
 
+# DP 테이블
+d = [0] * 30001
 
-# 모든 경우를 사용해서 1을 만들고 최소 횟수를 구하는 재귀함수
-def make_one(target, count):
-    # 1이 만들어지면 종료
-    if target == 1:
-        return count
-    # 5, 3, 2 로 나누거나 1을 뺸다.
-    result = 30000
-    if target % 5 == 0:
-        result = min(result, make_one(target // 5, count + 1))
-    if target % 3 == 0:
-        result = min(result, make_one(target // 3, count + 1))
-    if target % 2 == 0:
-        result = min(result, make_one(target // 2, count + 1))
-    result = min(result, make_one(target - 1, count + 1))
-    # 가장 작은 값을 출력한다.
-    return result
-
+# bottom-up 방식으로 구한다.
+for i in range(2, X + 1):
+    # 현재의 수에서 1을 빼는 경우
+    d[i] = d[i - 1] + 1
+    # 현재의 수가 2, 3, 5로 나누어 떨어지는 경우
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i // 2] + 1)
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i // 3] + 1)
+    if i % 5 == 0:
+        d[i] = min(d[i], d[i // 5] + 1)
 
 # 답을 구한다.
-print(make_one(X, 0))
+print(d[X])
